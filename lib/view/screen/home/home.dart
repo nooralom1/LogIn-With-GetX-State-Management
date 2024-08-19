af79d2134e77e996550819aed2553c5d79203320
NS NooR <non377938@gmail.com>
2024-08-19 23:33:08 +0600

    sign in

--- lib/view/screen/home/home.dart
+++ lib/view/screen/home/home.dart
@@ -10,7 +10,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Home"),),
-      body: Center(),
+      body: const Center(),
     );
   }
 }
