dd1ec78d441312fd339e0e203d30789ba0ed5be8
NS NooR <non377938@gmail.com>
2024-08-21 00:06:57 +0600

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:login_with_getx/view/screen/auth/sign_in/sign_in.dart';
 
-
-
-void main(){
+void main() {
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
