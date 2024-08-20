fc8ccda321e89ce2b3e0d5b004278e891fc6c315
NS NooR <non377938@gmail.com>
2024-08-21 00:05:39 +0600

    modified

--- lib/controller/getx_controller/sign_in.dart
+++ lib/controller/getx_controller/sign_in.dart
@@ -1,30 +1,24 @@
-
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:login_with_getx/controller/api_controller/sign_in_service.dart';
 import 'package:login_with_getx/view/screen/home/home.dart';
 
-
-class SignInController extends GetxController{
+class SignInController extends GetxController {
   final formKey = GlobalKey<FormState>();
   TextEditingController mailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   RxBool isLoading = false.obs;
 
-
-  signInFun()async{
+  signInFun() async {
     isLoading.value = true;
     bool status = await SignInService.signIn();
     isLoading.value = false;
 
-    if(status){
+    if (status) {
       log("Next Page");
-      Get.to(()=> const Home());
+      Get.to(() => const Home());
       return;
     }
-
-
   }
-
 }
