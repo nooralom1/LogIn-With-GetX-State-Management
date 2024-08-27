1fac54b44a60a6efccbbea032166e8c2882957b8
NS NooR <non377938@gmail.com>
2024-08-27 23:15:49 +0600

    product

--- lib/controller/getx_controller/splash.dart
+++ lib/controller/getx_controller/splash.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
+import 'package:login_with_getx/controller/local_storage/local_storage.dart';
 import 'package:login_with_getx/view/screen/auth/sign_up/sign_up.dart';
+import 'package:login_with_getx/view/screen/home/home.dart';
 
 class SplashController extends GetxController {
   // late AnimationController animationController;
@@ -13,8 +15,14 @@ class SplashController extends GetxController {
   }
 
   Future nextPage() async {
-    await Future.delayed(const Duration(seconds: 3));
-    Get.to(() => const SignUp());
+    String token =  await  LocalStorage().readData(key: "token") ?? "";
+    await Future.delayed(const Duration(seconds: 2));
+    if(token.isNotEmpty || token != ""){
+      Get.to(() => const Home());
+    }else{
+      Get.to(() => const SignUp());
+    }
+
   }
 
 // animationInitialization() {
