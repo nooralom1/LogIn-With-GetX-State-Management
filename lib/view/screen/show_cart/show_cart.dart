aabf3e9e2df3aaf7161dd09dad2f25bf12784151
NS NooR <non377938@gmail.com>
2024-08-28 12:49:33 +0600

    show cart

--- lib/view/screen/show_cart/show_cart.dart
+++ lib/view/screen/show_cart/show_cart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:login_with_getx/controller/api_controller/apies.dart';
 import 'package:login_with_getx/controller/getx_controller/show_cart.dart';
 
 class ShowCart extends StatefulWidget {
@@ -15,84 +16,92 @@ class _ShowCartState extends State<ShowCart> {
   Widget build(BuildContext context) {
     ShowCartController controller = Get.put(ShowCartController());
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
-          title: const Text(
-            "Show Cart Products",
-          ),
-          centerTitle: true,
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: const Text(
+          "Show Cart Products",
         ),
-        body: Obx(
-          () => controller.isLoading.isTrue
-              ? const Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Padding(
-                  padding: const EdgeInsets.only(top: 30),
-                  child: ListView.builder(
-                      itemCount: controller.productCart?.length,
-                      itemBuilder: (context, index) {
-                          return Container(
-                            margin: const EdgeInsets.all(10),
-                             color: Colors.green,
-                            child: Row(
-                              children: [
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("id: ${controller.productCart?[index].productId}"),
-                                    Text("Nmae: ${controller.productCart?[index].productName?.en}"),
-                                    Text("price: ${controller.productCart?.length}"),
-                                    Text("Quantity: ${controller.productCart?[index].quantity}"),
-                                    Row(
-                                      children: [
-                                        IconButton(
-                                            onPressed: () {
-                                              counter--;
-                                              setState(() {});
-                                            },
-                                            icon: const Icon(
-                                              Icons.remove,
-                                              size: 30,
-                                              color: Colors.deepOrange,
-                                            )),
-                                        const SizedBox(
-                                          width: 10,
-                                        ),
-                                        Text("$counter"),
-                                        const SizedBox(
-                                          width: 10,
-                                        ),
-                                        IconButton(
-                                            onPressed: () {
-                                              counter++;
-                                              setState(() {});
-                                            },
-                                            icon: const Icon(
-                                              Icons.add,
-                                              size: 30,
-                                              color: Colors.deepOrange,
-                                            ))
-                                      ],
-                                    ),
-                                    IconButton(
-                                        onPressed: () {
-                                          controller.deleteFromCart(id: controller.productCart?[index].productId ?? 0);
-                                        },
-                                        icon: const Icon(
-                                          Icons.delete_forever,
-                                          color: Colors.deepOrange,
-                                        )),
-
-                                  ],
-                                ),
-
-                              ],
+        centerTitle: true,
+      ),
+      body: Obx(
+        () => controller.isLoading.isTrue
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: ListView.builder(
+                  itemCount: controller.productCart?.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            height: 110,
+                              width: 120,
+                              child: Image.network("${Apies.baseUrl}/${controller.productCart?[index].productImage}",fit: BoxFit.fill,)),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("id: ${controller.productCart?[index].productId}"),
+                              Text("Nmae: ${controller.productCart?[index].productName?.en}"),
+                              Text("price: ${controller.productCart?[index].salePrice}"),
+                              Text("Quantity: ${controller.productCart?[index].quantity}"),
+                            ],
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              controller.deleteFromCart(
+                                  id: controller
+                                          .productCart?[index].productId ??
+                                      0);
+                            },
+                            icon: const Icon(
+                              Icons.delete_forever,
+                              color: Colors.deepOrange,
                             ),
-                          );
-                      }),
+                          ),
+                          Column(
+                            children: [
+                              IconButton(
+                                  onPressed: () {
+                                    counter++;
+                                    setState(() {});
+                                  },
+                                  icon: const Icon(
+                                    Icons.add,
+                                    size: 30,
+                                    color: Colors.deepOrange,
+                                  )),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Text("$counter"),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              IconButton(
+                                  onPressed: () {
+                                    counter--;
+                                    setState(() {});
+                                  },
+                                  icon: const Icon(
+                                    Icons.remove,
+                                    size: 30,
+                                    color: Colors.deepOrange,
+                                  )),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
-        ));
+              ),
+      ),
+    );
   }
 }
