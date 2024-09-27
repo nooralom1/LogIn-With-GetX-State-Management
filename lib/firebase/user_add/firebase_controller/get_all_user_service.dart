ec8f7a7c461cbb235f6700a7fc5e2b4c391182e2
NS NooR <non377938@gmail.com>
2024-09-27 21:15:43 +0600

    updated

--- lib/firebase/user_add/firebase_controller/get_all_user_service.dart
+++ lib/firebase/user_add/firebase_controller/get_all_user_service.dart
@@ -1,24 +1,23 @@
-
-
-
 import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class GetAllUserFirebaseService{
-
-  static Future<QuerySnapshot<Map<String, dynamic>>?> getUserService()async{
-    try{
-
+class GetAllUserFirebaseService {
+  static Future<QuerySnapshot<Map<String, dynamic>>?> getUserService() async {
+    try {
       FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
       // await firebaseFirestore.collection("user").add(data);
-       var d = DateTime.now();
-       log("$d");
-      QuerySnapshot<Map<String, dynamic>> aa =   await firebaseFirestore.collection("user").doc(d.toString().substring(0,11)).collection("aa").get();
-         log("=================== Data : $aa");
-         log("=================== Data : ${aa.docs.length}");
+      var d = DateTime.now();
+      log("$d");
+      QuerySnapshot<Map<String, dynamic>> aa = await firebaseFirestore
+          .collection("user")
+          .doc(d.toString().substring(0, 11))
+          .collection("aa")
+          .get();
+      log("=================== Data : $aa");
+      log("=================== Data : ${aa.docs.length}");
       return aa;
-    }catch (e){
+    } catch (e) {
       log("Error : $e");
     }
     return null;
