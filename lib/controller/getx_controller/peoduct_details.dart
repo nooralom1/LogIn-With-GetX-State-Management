5815c74f702e486037dd6f4233a78e28165c0086
NS NooR <non377938@gmail.com>
2024-09-27 21:15:40 +0600

    updated

--- lib/controller/getx_controller/peoduct_details.dart
+++ lib/controller/getx_controller/peoduct_details.dart
@@ -5,29 +5,28 @@ import 'package:login_with_getx/controller/api_controller/add_to_cart.dart';
 import 'package:login_with_getx/controller/api_controller/product_details.dart';
 import 'package:login_with_getx/model/product_details.dart';
 
-class ProductDetailsController extends GetxController{
+class ProductDetailsController extends GetxController {
   ProductDetails? productDetails;
   RxBool isLoading = false.obs;
   RxList<dynamic> image = [].obs;
 
-  getProductDetails()async{
-    isLoading.value=true;
-    var data  = await Get.arguments ?? 0;
-    productDetails = await ProductDetailsService.productDetailsService(id: data);
+  getProductDetails() async {
+    isLoading.value = true;
+    var data = await Get.arguments ?? 0;
+    productDetails =
+        await ProductDetailsService.productDetailsService(id: data);
     image(productDetails?.images);
-    isLoading.value=false;
+    isLoading.value = false;
   }
 
-  addToCartFunction({required int  id})async{
-   bool status = await AddToCartService.cartService(id: id);
-   log("============ AA : $status");
+  addToCartFunction({required int id}) async {
+    bool status = await AddToCartService.cartService(id: id);
+    log("============ AA : $status");
 
-   if(status == true){
-
+    if (status == true) {
       Get.back();
-   }
+    }
   }
-
 
   @override
   void onInit() {
