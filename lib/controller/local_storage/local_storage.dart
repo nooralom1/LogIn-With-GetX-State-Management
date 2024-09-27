eb536062addc6005dd380152a4fbdf87b4a361e1
NS NooR <non377938@gmail.com>
2024-09-27 21:15:34 +0600

    updated

--- lib/controller/local_storage/local_storage.dart
+++ lib/controller/local_storage/local_storage.dart
@@ -1,14 +1,13 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-class LocalStorage{
 
+class LocalStorage {
   final storage = const FlutterSecureStorage();
 
-  readData({required String key})async{
+  readData({required String key}) async {
     return await storage.read(key: key);
   }
 
-  writeData({required String key,required String value})async{
-    return await storage.write(key: key,value: value);
+  writeData({required String key, required String value}) async {
+    return await storage.write(key: key, value: value);
   }
-
 }
