b2f2193dbeb9bbdebeb0b34af1911a0e9f06744a
NS NooR <non377938@gmail.com>
2024-09-27 21:16:10 +0600

    updated

--- lib/controller/api_controller/products.dart
+++ lib/controller/api_controller/products.dart
@@ -4,17 +4,16 @@ import 'package:http/http.dart' as http;
 import 'package:login_with_getx/controller/api_controller/apies.dart';
 import 'package:login_with_getx/model/products.dart';
 
-class ProductsService{
-
-  static Future<List<Products>> productsService()async{
-    try{
+class ProductsService {
+  static Future<List<Products>> productsService() async {
+    try {
       Uri url = Uri.parse(Apies.productsUrl);
       var response = await http.get(url);
-      if(response.statusCode==200){
+      if (response.statusCode == 200) {
         ProductsModel data = ProductsModel.fromJson(jsonDecode(response.body));
-        return data.products??[];
+        return data.products ?? [];
       }
-    }catch(e){
+    } catch (e) {
       log("Error: $e");
     }
     return [];
