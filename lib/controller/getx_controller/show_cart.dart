cb30344bf1e513bd34aa52d488de6242384fe575
NS NooR <non377938@gmail.com>
2024-09-27 21:16:04 +0600

    updated

--- lib/controller/getx_controller/show_cart.dart
+++ lib/controller/getx_controller/show_cart.dart
@@ -3,30 +3,26 @@ import 'package:login_with_getx/controller/api_controller/delete_from_cart.dart'
 import 'package:login_with_getx/controller/api_controller/show_cart.dart';
 import 'package:login_with_getx/model/show_cart.dart';
 
-class ShowCartController extends GetxController{
+class ShowCartController extends GetxController {
   List<ProductCart>? productCart;
   RxBool isLoading = false.obs;
   RxInt counter = 1.obs;
 
-  getShowCart()async{
+  getShowCart() async {
     isLoading.value = true;
     productCart = await ShowCartService.showCartService();
     isLoading.value = false;
   }
 
-  deleteFromCart({required int id})async{
+  deleteFromCart({required int id}) async {
     isLoading.value = true;
-    bool status  = await DeleteFromCartService.cartService(id: id);
+    bool status = await DeleteFromCartService.cartService(id: id);
     isLoading.value = false;
 
-    if(status){
+    if (status) {
       getShowCart();
     }
-
   }
-
-
-
 
   @override
   void onInit() {
